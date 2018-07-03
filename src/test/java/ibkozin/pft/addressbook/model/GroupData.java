1be3b9b06808079b71a3f5042aeda585f3c74adf
ibkozin <ibkozin@gmail.com>
2018-07-02 19:01:08 -0500

    Лекция 4.7. Множества: неупорядоченные коллекции

--- src/test/java/ibkozin/pft/addressbook/model/GroupData.java
+++ src/test/java/ibkozin/pft/addressbook/model/GroupData.java
@@ -6,11 +6,24 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
+    private final String id;
 
-    public GroupData(String name, String header, String footer) {
+    public GroupData(String name, String header, String footer)
+    {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+    public GroupData(String id, String name, String header, String footer)
+    {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -29,6 +42,7 @@ public class GroupData {
     public String toString() {
         return "GroupData{" +
                 "name='" + name + '\'' +
+                ", id='" + id + '\'' +
                 '}';
     }
 
@@ -37,12 +51,13 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name) &&
+                Objects.equals(id, groupData.id);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name);
+        return Objects.hash(name, id);
     }
 }
