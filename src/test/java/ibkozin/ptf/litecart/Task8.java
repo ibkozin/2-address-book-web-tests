7ff4360fee9b456f1137a1e3280ec992f4e7c468
ibkozin <ibkozin@gmail.com>
2018-07-04 10:40:45 -0500

    Task 8 с динамическим списком

--- src/test/java/ibkozin/ptf/litecart/Task8.java
+++ src/test/java/ibkozin/ptf/litecart/Task8.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Task8 {
@@ -23,16 +25,22 @@ public class Task8 {
     @Test
     public void task8(){
         driver.navigate().to("http://localhost/litecart/");
-        areElementsPresent(By.cssSelector("a.link[title='Purple Duck']"));
-        areElementsPresent(By.cssSelector("a.link[title='Yellow Duck']"));
-        areElementsPresent(By.cssSelector("a.link[title='Blue Duck']"));
-        areElementsPresent(By.cssSelector("a.link[title='Red Duck']"));
-        areElementsPresent(By.cssSelector("a.link[title='Green Duck']"));
+//        areElementsPresent(By.cssSelector("a.link[title='Purple Duck']"));
+//        areElementsPresent(By.cssSelector("a.link[title='Yellow Duck']"));
+//        areElementsPresent(By.cssSelector("a.link[title='Blue Duck']"));
+//        areElementsPresent(By.cssSelector("a.link[title='Red Duck']"));
+//        areElementsPresent(By.cssSelector("a.link[title='Green Duck']"));
+          List<WebElement> products = driver.findElements(By.cssSelector("[class^=listing] a.link"));
+          for (WebElement product : products){
+              List<WebElement> stickers = product.findElements(By.cssSelector("div.sticker"));
+              Assert.assertEquals(stickers.size(), 1);
+          }
+
     }
-    boolean areElementsPresent(By locator) {
-        WebElement product = driver.findElement(locator);
-        return product.findElements(By.cssSelector("div.sticker")).size() == 1;
-    }
+//    boolean areElementsPresent(By locator) {
+//        WebElement product = driver.findElement(locator);
+//        return product.findElements(By.cssSelector("div.sticker")).size() == 1;
+//    }
 
 
 
